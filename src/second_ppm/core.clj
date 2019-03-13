9a04e4611188db6be7d9fb354e5ca3e2019e3257
VikramShawFL <vikram@front.live>
2019-03-13 16:27:38 +0530

    Second ppm

---
+++ src/second_ppm/core.clj
@@ -0,0 +1,19 @@
+(ns second-ppm.core)
+
+(def head (str "P3\n" 255 " " 255 " " 255 "\n"))
+
+(def matrix
+  (str
+   head (clojure.string/join
+         "\n"
+            (mapcat (fn [i]
+                      (map (fn [j]
+                             (let [x (mod i 255), y (mod j 255), z (mod (* i j) 255)]
+                               (str x " " y " " z)))
+                         (range (dec 255) -1 -1)))
+                    (range 0 255)))))
+
+
+(def second-ppm matrix)
+
+(spit "image.ppm" second-ppm)
---
+++ test/second_ppm/core_test.clj
@@ -0,0 +1,7 @@
+(ns second-ppm.core-test
+  (:require [clojure.test :refer :all]
+            [second-ppm.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
